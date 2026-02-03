484ce8bc4db0d911aca681af967a553f627fdfc2
Prashant Kumar Prajapati <prashant.prajapati@rapipay.com>
2026-02-03 18:47:24 +0530

    initial commit

---
+++ src/main/kotlin/com/SGSP/SGSP_WEBSITE_CENTER/SgspWebsiteCenterApplication.kt
@@ -0,0 +1,11 @@
+package com.SGSP.SGSP_WEBSITE_CENTER
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SgspWebsiteCenterApplication
+
+fun main(args: Array<String>) {
+	runApplication<SgspWebsiteCenterApplication>(*args)
+}
---
+++ src/test/kotlin/com/SGSP/SGSP_WEBSITE_CENTER/SgspWebsiteCenterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.SGSP.SGSP_WEBSITE_CENTER
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SgspWebsiteCenterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
